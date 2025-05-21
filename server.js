ceee04b576aa496253a292f1afba485d918af2df
Kshitij <xeeteexstha@gmail.com>
2025-05-21 23:53:00 +0545

    minor update

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
-import cors from "cors"
+
 import productRoute from "./routes/product.route.js"
 
 const app = express();
 
-app.use(cors());
 
 app.use(express.json()); // middleware
 
